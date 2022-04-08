698d225bf9ff9b6a67b33946e284b50ba9fbe405
shalonningram1 <shalonn.ingram@cloudreach.com>
2022-04-08 11:45:54 -0400

    formatting terraform files

--- backend.tf
+++ backend.tf
@@ -3,6 +3,6 @@ terraform {
     bucket         = "talent-academy-tf-backend-shalonn"
     key            = "terraform/ec2/ec2-terraform.tfstates"
     dynamodb_table = "terraform-lock"
-    region = "us-east-1"
+    region         = "us-east-1"
   }
 }
